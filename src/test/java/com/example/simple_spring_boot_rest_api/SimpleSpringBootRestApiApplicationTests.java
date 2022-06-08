fe77152855b39d5c0cee8754ef849e9b26a73bef
johnhtml <jogerenag@unal.edu.co>
2022-06-08 14:38:53 -0500

    Added the link used to get the initial bootstrapped version

--- src/test/java/com/example/simple_spring_boot_rest_api/SimpleSpringBootRestApiApplicationTests.java
+++ src/test/java/com/example/simple_spring_boot_rest_api/SimpleSpringBootRestApiApplicationTests.java
@@ -10,4 +10,6 @@ class SimpleSpringBootRestApiApplicationTests {
 	void contextLoads() {
 	}
 
+	// this project was bootstrapped at
+	// https://start.spring.io/#!type=maven-project&language=java&platformVersion=2.7.0&packaging=jar&jvmVersion=11&groupId=com.example&artifactId=simple_spring_boot_rest_api&name=simple_spring_boot_rest_api&description=Demo%20project%20for%20Spring%20Boot&packageName=com.example.simple_spring_boot_rest_api&dependencies=web
 }
